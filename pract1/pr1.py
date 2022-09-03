b864848f0ee8c036fa2973e007ae5fe20083f552
graagm <graagm@gmail.com>
2022-09-03 10:51:34 +0300

    practic1

---
+++ pract1/pr1.py
@@ -0,0 +1,40 @@
+# Задача1
+
+# s = int(input())
+# s2 = int(input())
+
+# if s ** 2 == s2:
+#     print('да')
+# else:
+#     print('нет')
+
+
+# Задача2
+# ls = []
+# for _ in range(5):
+#     ls.append(int(input()))
+
+# print(max(ls))
+
+
+# Задача3
+# s = int(input())
+# for _ in range(-s, s+1):
+#     print(_, end=' ')
+
+
+
+
+
+
+# Задача5
+# s = int(input())
+
+
+# if any(s % i == 0  for i in [5, 10, 15]) and s % 30:
+#     print('да')
+# else:
+#     print('нет')
+
+
+
