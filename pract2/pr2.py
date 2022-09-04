4eb91914cc4612866b923d232ae21f69e3eb5200
graagm <graagm@gmail.com>
2022-09-04 11:56:47 +0300

    homework ready

---
+++ pract2/pr2.py
@@ -0,0 +1,64 @@
+# Задача1
+
+# s = input()
+# sm = 0
+# ls = (i for i in s if i.isdigit())
+# print(sum(int(i) for i in ls))
+
+
+# Задача2
+
+# def fct(x):
+#     if x == 0:
+#         return 1
+#     return x * fct(x-1)
+
+# n  = int(input())
+
+# ls = [fct(i) for i in range(1, n+1)]
+
+# print(ls)
+
+
+# Задача3
+
+# n  = int(input())
+# print({i: round((1 * 1 / i) ** i, 4) for i in range(1, n+1)})
+
+
+
+# Задача4
+# from random import randint
+
+# from itertools import cycle
+
+# ml = 1
+
+# file = open('file.txt', 'r', encoding='UTF-8')
+
+# n  = int(input())
+
+# ls = [randint(-n, n) for _ in range(n+1)]
+
+# listt = cycle(ls)
+
+# for i in file:
+#     try:
+#         ml *= int(i[next(listt)])
+#     except:
+#         continue
+
+
+# print(ml)
+# file.close()
+
+
+# Задача5
+
+# from random import shuffle, randint
+
+# n  = int(input())
+
+# ls = [randint(-n, n) for _ in range(n+1)]
+# shuffle(ls)
+# print(ls)
