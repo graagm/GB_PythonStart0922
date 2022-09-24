b27ef0162731e9ddadf2ffa8490ac47bb42f8cb9
graagm <graagm@gmail.com>
2022-09-24 15:31:05 +0300

    first version

--- pract7/tel_guide/conf/guides_phones/urls.py
+++ pract7/tel_guide/conf/guides_phones/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HeadMain
+from .views import HeadMain, UsersPhoneCreateView, UsersPhoneListView, \
+                    UsersPhoneDetailView, UsersPhoneUpdateView
 
 urlpatterns = [
-        path('', HeadMain, name='Head'),
+    path('', HeadMain, name='Head'),
+    path('add', UsersPhoneCreateView.as_view(), name='add_user'),
+    path('all', UsersPhoneListView.as_view(), name='all_users'),
+    path('user/<int:pk>', UsersPhoneDetailView.as_view(), name='one_user'),
+    path('user/<int:pk>/update', UsersPhoneUpdateView.as_view(), name='user_update'),
 ]
--- pract7/tel_guide/conf/guides_phones/views.py
+++ pract7/tel_guide/conf/guides_phones/views.py
@@ -3,8 +3,32 @@ from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView, DetailView
 from .models import UsersPhone
+from .forms import UsersPhoneForm
 
 
 # Create your views here.
 def HeadMain(request):
     return render (request, 'tel_giude/index.html')
+
+class UsersPhoneCreateView(CreateView):
+    template_name = 'tel_giude/add_user.html'
+    form_class = UsersPhoneForm
+    model = UsersPhone
+    success_url = '/guides_phones'
+
+class UsersPhoneListView(ListView):
+    model = UsersPhone
+    template_name = 'tel_giude/all_users.html'
+    context_object_name = 'users_list'
+
+class UsersPhoneDetailView(DetailView):
+    model = UsersPhone
+    template_name = 'tel_giude/one_user.html'
+    context_object_name = "user" 
+
+class UsersPhoneUpdateView(UpdateView):
+    model = UsersPhone
+    form_class = UsersPhoneForm
+    template_name = 'tel_giude/update_user.html'
+    context_object_name = "user" 
+    success_url = '/guides_phones'
